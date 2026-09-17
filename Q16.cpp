8ae16a57c912803755a2a8f6131fc0b125cedb71
ashu07khare-coder <ashu07khare@gmail.com>
2026-09-17 16:26:56 +0530

    Q16.cpp

---
+++ Q16.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n1,n2=0;
+    cout<<"Enter a number: ";
+    cin>>n1;
+    int original=n1;
+    while(n1>0){
+        n2=(n2*10)+(n1%10);
+        n1/=10;
+    }
+    if(n2==original){
+        cout<<"Palindrome";
+    }
+    else{
+        cout<<"Not a palindrome";
+    }
+
+}
